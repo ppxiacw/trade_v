ac63f3e0b42b73eb31f2b3d788096063c979f6d7
曹威 <970488001@qq.com>
2025-09-28 22:05:53 +0800

    1

--- monitor/services/stock_monitor.py
+++ monitor/services/stock_monitor.py
@@ -1,7 +1,7 @@
 import concurrent.futures
 import threading
 import time
-from datetime import datetime
+from datetime import datetime, time as dt_time
 
 
 class StockMonitor:
@@ -13,6 +13,14 @@ class StockMonitor:
         self.stock_data = stock_data
         self.lock = threading.Lock()
 
+    def is_market_open(self):
+        """检查当前时间是否在股票市场的开盘时间内"""
+        now = datetime.now().time()
+        # 假设开盘时间为 9:30 AM 到 4:00 PM
+        market_open = dt_time(11, 31)
+        market_close = dt_time(12, 59)
+        return market_open <= now <= market_close
+
     def start_monitoring(self):
         print(f"开始监控 {len(self.config.MONITOR_STOCKS)} 个")
         stock_codes = list(self.config.MONITOR_STOCKS.keys())
@@ -21,24 +29,26 @@ class StockMonitor:
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=10)
 
         while True:
+            # 检查市场是否开盘
+            if self.is_market_open():
+                print("市场已收盘，停止监控")
+                time.sleep(60)  # 每分钟检查一次市场是否重新开盘
+                continue
 
-                # 获取数据
-                data_list = self.data_fetcher.fetch_realtime_data(stock_codes)
+            # 获取数据
+            data_list = self.data_fetcher.fetch_realtime_data(stock_codes)
 
-                if not data_list:
-                    time.sleep(1)  # 数据为空时短暂休眠
-                    continue
 
-                # 并行处理数据更新和警报检查
-                with self.lock:
-                    self.stock_data.update_data(data_list)
+            # 并行处理数据更新和警报检查
+            with self.lock:
+                self.stock_data.update_data(data_list)
 
-                futures = []
-                for stock in stock_codes:
-                    futures.append(executor.submit(self.check_and_send_alerts, stock))
+            futures = []
+            for stock in stock_codes:
+                futures.append(executor.submit(self.check_and_send_alerts, stock))
 
-                # 等待所有检查完成
-                concurrent.futures.wait(futures)
+            # 等待所有检查完成
+            concurrent.futures.wait(futures)
 
 
 
